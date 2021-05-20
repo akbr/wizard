33e3c60e393e6c00dda948063fbf8c98195b0b4f
Aaron Rieke <akbr@akbr.net>
2021-05-20 14:15:27 -0400

    dev/propduction ws toggle

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import { initConnection } from "./server/initConnection";
 import { initState } from "./state/initState";
 import { initView } from "./views/initView";
 
-let manager = initConnection("ws://localhost:5000");
-//let manager = initConnection(location.origin.replace(/^http/, "ws"));
+let manager =
+  location.hostname !== "localhost"
+    ? initConnection(location.origin.replace(/^http/, "ws"))
+    : initConnection("ws://localhost:5000");
+
 //let [manager, server] = initConnection();
 
 let store = initState(manager);

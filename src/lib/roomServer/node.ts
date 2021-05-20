0717e98af818ddde36a753c87f9cd14ec76fbdc7
Aaron Rieke <akbr@akbr.net>
2021-05-20 12:02:39 -0400

    heartbeat attempt

--- src/lib/roomServer/node.ts
+++ src/lib/roomServer/node.ts
@@ -1,15 +1,24 @@
 import * as express from "express";
-import { Server } from "ws";
+import { Server, WebSocket } from "ws";
 import { Cartridge } from "./types";
 import { createRoomServer } from "./";
+
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1234;
+const distPath = path.resolve("dist/");
+
+function noop() {}
+
+function heartbeat() {
+  //@ts-ignore
+  this.isAlive = true;
+}
 
 export function createNodeServer(cartridge: Cartridge<any, any, any>) {
   let server = express()
-    .use(express.static(path.resolve("dist/")))
+    .use(express.static(distPath))
     .get("/", function (_, res) {
-      res.sendFile("index.html", { root: path.resolve("dist/") });
+      res.sendFile("index.html", { root: distPath });
     })
     .listen(PORT, function () {
       return console.log("Listening on " + PORT);
@@ -18,11 +27,26 @@ export function createNodeServer(cartridge: Cartridge<any, any, any>) {
   let gameServer = createRoomServer(cartridge);
   const wss = new Server({ server });
 
+  const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+      //@ts-ignore
+      if (ws.isAlive === false) return ws.terminate();
+      //@ts-ignore
+      ws.isAlive = false;
+      ws.ping(noop);
+    });
+  }, 25000);
+
   function createRemote(wsss: typeof wss) {
     wsss.on("connection", function (ws) {
       let socket: any = {
         send: (msg: any) => ws.send(JSON.stringify(msg)),
       };
+
+      //@ts-ignore
+      ws.isAlive = true;
+      //@ts-ignore
+      ws.on("pong", heartbeat);
 
       gameServer.onOpen(socket);
 
@@ -38,6 +62,10 @@ export function createNodeServer(cartridge: Cartridge<any, any, any>) {
       });
     });
   }
+
+  wss.on("close", function close() {
+    clearInterval(interval);
+  });
 
   createRemote(wss);
 }
